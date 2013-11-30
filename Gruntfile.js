9f59458f7fe8b23f0dde1f67197b5d6c55feeec1
Bartek Tomżyński <bartlomiej@tomzynski.com>
2013-11-30 19:14:37 +0100

    less compilation by grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,32 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          paths: ["less/"]
+        },
+        files: {
+          "css/layout.css": "less/layout.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["less/"],
+          cleancss: true
+        },
+        files: {
+          "css/layout.css": "less/layout.less"
+        }
+      }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
   // grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.loadNpmTasks('grunt-contrib-less');
 
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
